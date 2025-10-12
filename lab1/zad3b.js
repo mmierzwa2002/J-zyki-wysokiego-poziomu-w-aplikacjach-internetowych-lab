d572fd03b89ee7d8f990f351b06664e6c92bb191
Michał Mierzwa <123924536+mmierzwa2002@users.noreply.github.com>
2025-10-12 19:01:32 +0200

    Add zad3b.js (contains util.log is not a function error, as it was removed in newer versions of node.js)

---
+++ lab1/zad3b.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const path = require('path');
+const util = require('util');
+const v8 = require('v8');
+
+const hostname = '127.0.0.1';
+const port = 8888;
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end("Hello, from node.js server!\n");
+});
+
+server.listen(port, hostname, () => {
+    util.log(v8.getHeapStatistics());
+    console.log(path.basename(__filename));
+    util.log(path.join(__dirname, 'uploads', 'images'));
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
