4d6b40587a8f42f312cbab0170a0b8d3fa34a4e3
Michał Mierzwa <123924536+mmierzwa2002@users.noreply.github.com>
2025-10-12 19:10:58 +0200

    Add zad4.js

---
+++ lab1/zad4.js
@@ -0,0 +1,5 @@
+//Użyto moduł o nazwie repeat-string który powtarza wybrany łańcuch wybraną przez użytkownika ilość razy
+let repeat = require('repeat-string');
+console.log(`Użyto moduł o nazwie repeat-string który powtarza wybrany łańcuch wybraną przez użytkownika ilość razy za pomocą funkcji repeat("x", y), gdzie x to łańuch a y to ilość powtórzeń`);
+console.log('Przykład użycia, powtórzono łańcuch "Hello World!" 10 razy:');
+console.log(repeat('Hello World! ', 10));
