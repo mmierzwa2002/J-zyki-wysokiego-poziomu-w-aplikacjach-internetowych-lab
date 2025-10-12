76f68e92b207515075d87777f333b23973f61958
Michał Mierzwa <123924536+mmierzwa2002@users.noreply.github.com>
2025-10-12 18:47:34 +0200

    Add zad2b.js

---
+++ zad2b.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+console.log("Początek skryptu.");
+
+fs.readFile('package.json', function (err, data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+});
+
+console.log("Koniec skryptu.");
