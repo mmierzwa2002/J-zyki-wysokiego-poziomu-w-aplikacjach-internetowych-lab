16c85eaecdb2899a3837aec89730367685d57163
Michał Mierzwa <123924536+mmierzwa2002@users.noreply.github.com>
2025-10-20 02:43:38 +0200

    Fix indentation:

--- lab2/index.js
+++ lab2/index.js
@@ -1,26 +1,28 @@
-const http = require('http');
-const fs = require('fs');
-const server = http.createServer(function (req, res) {
- getTitles(res);
-}).listen(8000, "127.0.0.1");
+const http = require("http");
+const fs = require("fs");
+const server = http
+  .createServer(function (req, res) {
+    getTitles(res);
+  })
+  .listen(8000, "127.0.0.1");
 function getTitles(res) {
-fs.readFile('./titles.json', function (err, data) {
-if (err) return hadError(err, res);
-getTemplate(JSON.parse(data.toString()), res);
-});
+  fs.readFile("./titles.json", function (err, data) {
+    if (err) return hadError(err, res);
+    getTemplate(JSON.parse(data.toString()), res);
+  });
 }
 function getTemplate(titles, res) {
- fs.readFile('./template.html', function (err, data) {
- if (err) return hadError(err, res);
- formatHtml(titles, data.toString(), res);
- });
+  fs.readFile("./template.html", function (err, data) {
+    if (err) return hadError(err, res);
+    formatHtml(titles, data.toString(), res);
+  });
 }
 function formatHtml(titles, tmpl, res) {
- let html = tmpl.replace('%', titles.join('</li><li>'));
- res.writeHead(200, {'Content-Type': 'text/html'});
- res.end(html);
+  let html = tmpl.replace("%", titles.join("</li><li>"));
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(html);
 }
 function hadError(err, res) {
- console.error(err);
- res.end('Error');
+  console.error(err);
+  res.end("Error");
 }
