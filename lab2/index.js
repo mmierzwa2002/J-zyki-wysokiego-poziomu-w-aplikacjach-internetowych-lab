1901b8d2b22930009ab258d0c7c6d569d0e0a095
Michał Mierzwa <123924536+mmierzwa2002@users.noreply.github.com>
2025-10-20 02:39:45 +0200

    End getTitles function early if error occurs

--- lab2/index.js
+++ lab2/index.js
@@ -4,14 +4,10 @@ const server = http.createServer(function (req, res) {
  getTitles(res);
 }).listen(8000, "127.0.0.1");
 function getTitles(res) {
- fs.readFile('./titles.json', function (err, data) {
- if (err) {
- hadError(err, res);
- }
- else {
- getTemplate(JSON.parse(data.toString()), res);
- }
- });
+fs.readFile('./titles.json', function (err, data) {
+if (err) return hadError(err, res);
+getTemplate(JSON.parse(data.toString()), res);
+});
 }
 function getTemplate(titles, res) {
  fs.readFile('./template.html', function (err, data) {
